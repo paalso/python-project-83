7eed4f1a2b9e7646a0819144fe27103a12c7aec4
Pavel Soroka <40a.pvl@gmail.com>
2025-02-13 17:33:45 +0200

    Start implement templastes

--- page_analyzer/routes.py
+++ page_analyzer/routes.py
@@ -1,8 +1,16 @@
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    render_template
+)
 
 routes = Blueprint('routes', __name__)
 
 @routes.route('/')
 @routes.route('/index')
 def index():
-    return 'Hello from Flask!'
+    return render_template('index.html')
+
+
+@routes.route('/urls')
+def urls():
+    return 'url list'
