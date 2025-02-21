a69c35253a722b6304ebb9c21f789203776d15b7
Pavel Soroka <40a.pvl@gmail.com>
2025-02-21 13:25:17 +0200

    Implement messaging about errors on the index page

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,12 @@
 from flask import Flask
 from page_analyzer.routes import routes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
+
 app.register_blueprint(routes)
---
+++ page_analyzer/validator.py
@@ -0,0 +1,12 @@
+import validators
+
+
+MAX_URL_LEN = 255
+
+def validate_url(url):
+    errors = {}
+    if len(url) > MAX_URL_LEN:
+        errors.setdefault('url', []).append('URL превышает 255 символов')
+    elif validators.url(url) is not True:
+        errors.setdefault('url', []).append('Некорректный URL')
+    return errors
