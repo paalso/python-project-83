1b1bb5f923448fcc599ed80986f0cfd6e87e920e
Pavel Soroka <40a.pvl@gmail.com>
2025-02-24 14:38:15 +0200

    Implement adding new url

--- page_analyzer/routes.py
+++ page_analyzer/routes.py
@@ -2,17 +2,30 @@ from flask import (
     Blueprint,
     flash,
     get_flashed_messages,
+    jsonify,
     render_template,
     request
 )
 from page_analyzer.db import get_db
 from page_analyzer.repository import Repository
 from page_analyzer.validator import validate_url
+from urllib.parse import urlparse
 
 
 def flash_errors(errors, key):
     for error in errors.get(key, []):
         flash(error, 'error')
+
+
+def normalize_url(url):
+    parsed_url = urlparse(url)
+    url_scheme = parsed_url.scheme
+    url_host = parsed_url.hostname
+
+    if not url_scheme or not url_host:
+        return None
+
+    return f"{url_scheme}://{url_host}"
 
 
 routes = Blueprint('routes', __name__)
@@ -34,7 +47,8 @@ def urls():
 
 @routes.route('/urls/<int:id>')
 def url_info(id):
-    return render_template('/url.html')
+    url = repo.find(id)
+    return url
 
 
 @routes.post('/urls')
@@ -47,13 +61,36 @@ def urls_post():
         messages = get_flashed_messages(with_categories=True)
         return render_template(
             'index.html',
+            url=url,
             messages=messages
         )
-    return 'All right!!!'
+
+    normalized_url = normalize_url(url)
+    new_url_record = repo.create(normalized_url)
+    flash('Страница успешно добавлена', 'success')
+    if new_url_record:
+        return render_template(
+            '/url.html',
+            url_record=new_url_record,
+            messages = get_flashed_messages(with_categories=True)
+        )
+    return 'Already exists'
+
+
+@routes.post('/urls/<int:id>/checks')
+def check_url(id):
+    return f'Checking url with {id}...'
 
 
 # TODO: remove after debugging
 @routes.route('/conn')
 def get_conn():
-    conn = get_db()
     return str(id(conn))
+
+
+@routes.route('/urls/find/<path:url>')
+def url_by_name(url):
+    result = repo.find_by_field("name", url)
+    if result:
+        return jsonify(result), 200
+    return jsonify({"error": "URL not found"}), 404
