6c228d9fd98e90ef998f41a2e9825ca1b8132954
Pavel Soroka <40a.pvl@gmail.com>
2025-02-26 12:36:33 +0200

    Implement url post logic

--- page_analyzer/repositories/urls_repository.py
+++ page_analyzer/repositories/urls_repository.py
@@ -1,3 +1,4 @@
+import psycopg2
 from psycopg2.extras import DictCursor
 
 from page_analyzer.repositories.base_repository import BaseRepository
@@ -8,17 +9,13 @@ class UrlsRepository(BaseRepository):
     def table_name(self):
         return 'urls'
 
-    def save(self, url):
-        existing = self._find_by_field('name', url)
-
-        if existing:
-            return
-
-        with self.conn.cursor(cursor_factory=DictCursor) as cursor:
-            cursor.execute(
-                'INSERT INTO urls (name) VALUES (%s) RETURNING *',
-                (url,)
-            )
-            new_record = cursor.fetchone()
-            self.conn.commit()
+    def _create(self, entity):
+        try:
+            with self.conn.cursor(cursor_factory=DictCursor) as cur:
+                cur.execute('INSERT INTO urls (name) VALUES (%(url)s) RETURNING *', entity)
+                new_record = cur.fetchone()
+                self.conn.commit()
             return new_record
+        except psycopg2.Error as e:
+            self.conn.rollback()
+            print(f"Database error: {e}")
