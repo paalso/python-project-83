82d769fade7aaba4558998c78c37f916cf17bcea
Pavel Soroka <40a.pvl@gmail.com>
2025-03-03 15:18:24 +0200

    UPD: change connection to db logic

--- page_analyzer/repositories/url_checks_repository.py
+++ page_analyzer/repositories/url_checks_repository.py
@@ -9,10 +9,9 @@ class UrlChecksRepository(BaseRepository):
     def table_name(self):
         return 'url_checks'
 
-
     def get_by_url_id(self, url_id):
-        with self.conn:
-            with self.conn.cursor(cursor_factory=DictCursor) as cur:
+        with self._get_connection() as conn:
+            with conn.cursor(cursor_factory=DictCursor) as cur:
                 cur.execute('''
                     SELECT
                       uc.id, uc.status_code, uc.h1, uc.title, uc.description, uc.created_at
@@ -28,8 +27,8 @@ class UrlChecksRepository(BaseRepository):
 
     def _create(self, entity):
         try:
-            with self.conn:
-                with self.conn.cursor(cursor_factory=DictCursor) as cur:
+            with self._get_connection() as conn:
+                with conn.cursor(cursor_factory=DictCursor) as cur:
                     cur.execute('''
                         INSERT INTO url_checks (url_id, status_code, h1, title, description)
                         VALUES (%(url_id)s, %(status_code)s, %(h1)s, %(title)s, %(description)s)
