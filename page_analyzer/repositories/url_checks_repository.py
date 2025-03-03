2a40ee4135e1a4b1f930983f95374670369ee98f
Pavel Soroka <40a.pvl@gmail.com>
2025-03-03 11:42:33 +0200

    UPD db connecting

--- page_analyzer/repositories/url_checks_repository.py
+++ page_analyzer/repositories/url_checks_repository.py
@@ -11,33 +11,31 @@ class UrlChecksRepository(BaseRepository):
 
 
     def get_by_url_id(self, url_id):
-        with self.conn.cursor(cursor_factory=DictCursor) as cursor:
-            cursor.execute('''
-                SELECT
-                  uc.id, uc.status_code, uc.h1, uc.title, uc.description, uc.created_at
-                FROM url_checks uc
-                INNER JOIN urls u
-                ON u.id = uc.url_id
-                WHERE uc.url_id = %s
-                ORDER BY uc.created_at DESC;''',
-                (url_id,)
-            )
-            records = cursor.fetchall()
-        return records
-
+        with self.conn:
+            with self.conn.cursor(cursor_factory=DictCursor) as cur:
+                cur.execute('''
+                    SELECT
+                      uc.id, uc.status_code, uc.h1, uc.title, uc.description, uc.created_at
+                    FROM url_checks uc
+                    INNER JOIN urls u
+                    ON u.id = uc.url_id
+                    WHERE uc.url_id = %s
+                    ORDER BY uc.created_at DESC;''',
+                    (url_id,)
+                )
+                records = cur.fetchall()
+            return records
 
     def _create(self, entity):
         try:
-            with self.conn.cursor(cursor_factory=DictCursor) as cur:
-                cur.execute('''
-                    INSERT INTO url_checks (url_id, status_code, h1, title, description)
-                    VALUES (%(url_id)s, %(status_code)s, %(h1)s, %(title)s, %(description)s)
-                    RETURNING *''',
-                    entity
-                )
-                new_record = cur.fetchone()
-                self.conn.commit()
+            with self.conn:
+                with self.conn.cursor(cursor_factory=DictCursor) as cur:
+                    cur.execute('''
+                        INSERT INTO url_checks (url_id, status_code, h1, title, description)
+                        VALUES (%(url_id)s, %(status_code)s, %(h1)s, %(title)s, %(description)s)
+                        RETURNING *''', entity)
+                    new_record = cur.fetchone()
             return new_record
         except psycopg2.Error as e:
-            self.conn.rollback()
             print(f"Database error: {e}")
+            return None
--- page_analyzer/repositories/urls_repository.py
+++ page_analyzer/repositories/urls_repository.py
@@ -15,26 +15,27 @@ class UrlsRepository(BaseRepository):
 
         :return: List of dictionaries representing enriched URL records.
         """
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("""
-                SELECT DISTINCT ON (u.id)
-                    u.id,
-                    u.name,
-                    uc.created_at AS url_last_check_time,
-                    uc.status_code
-                FROM urls u
-                LEFT JOIN url_checks uc ON u.id = uc.url_id
-                ORDER BY u.id DESC, uc.created_at DESC NULLS LAST
-            """)
-            return [dict(row) for row in cur]
+        with self.conn:
+            with self.conn.cursor(cursor_factory=DictCursor) as cur:
+                cur.execute("""
+                    SELECT DISTINCT ON (u.id)
+                        u.id,
+                        u.name,
+                        uc.created_at AS url_last_check_time,
+                        uc.status_code
+                    FROM urls u
+                    LEFT JOIN url_checks uc ON u.id = uc.url_id
+                    ORDER BY u.id DESC, uc.created_at DESC NULLS LAST
+                """)
+                return [dict(row) for row in cur]
 
     def _create(self, entity):
         try:
-            with self.conn.cursor(cursor_factory=DictCursor) as cur:
-                cur.execute('INSERT INTO urls (name) VALUES (%(url)s) RETURNING *', entity)
-                new_record = cur.fetchone()
-                self.conn.commit()
+            with self.conn:
+                with self.conn.cursor(cursor_factory=DictCursor) as cur:
+                    cur.execute('INSERT INTO urls (name) VALUES (%(url)s) RETURNING *', entity)
+                    new_record = cur.fetchone()
             return new_record
         except psycopg2.Error as e:
-            self.conn.rollback()
             print(f"Database error: {e}")
+            return None
