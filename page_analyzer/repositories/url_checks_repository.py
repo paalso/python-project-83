8c253cb7e99e9da8c5fa4036e2f7f821b58272b5
Pavel Soroka <40a.pvl@gmail.com>
2025-03-05 18:11:40 +0200

    Fix the rest linter errors (in page_analyzer/repositories/url_checks_repository.py)

--- page_analyzer/repositories/url_checks_repository.py
+++ page_analyzer/repositories/url_checks_repository.py
@@ -31,8 +31,10 @@ class UrlChecksRepository(BaseRepository):
             with self._get_connection() as conn:
                 with conn.cursor(cursor_factory=DictCursor) as cur:
                     cur.execute('''
-                        INSERT INTO url_checks (url_id, status_code, h1, title, description)
-                        VALUES (%(url_id)s, %(status_code)s, %(h1)s, %(title)s, %(description)s)
+                        INSERT INTO url_checks 
+                          (url_id, status_code, h1, title, description)
+                        VALUES (%(url_id)s, %(status_code)s, 
+                                %(h1)s, %(title)s, %(description)s)
                         RETURNING *''', entity)
                     new_record = cur.fetchone()
             return new_record
